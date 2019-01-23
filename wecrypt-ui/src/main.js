238f1fc2d98fc888a5963d91132a5dd2df879b03
Ambrose Wheatcroft <ambrosewheatcroft@gmail.com>
2019-01-23 11:55:28 -0500

    save

--- wecrypt-ui/src/main.js
+++ wecrypt-ui/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from 'axios'
 Vue.config.productionTip = false
+
+Vue.use({
+    install (Vue) {
+        Vue.prototype.$server = axios.create({
+            baseURL: 'http://localhost:8123/'
+        })
+    }
+})
 
 new Vue({
   render: h => h(App),
